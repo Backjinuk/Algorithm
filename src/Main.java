adf4e40efc07491def092415bca222741d27e8f7
mac <backj123@naver.com>
2024-12-25 17:03:45 +0900

    디렉토리 이동

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,10 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int x = scanner.nextInt();
-        int n = 0;
-        int position = 0;
-
-        while(x > 0){
-           n++;
-           x -= n;
-        }
-
-        position = x + n;
-
-
-
-        int molecule , denominator;
-
-        if(n % 2 == 0){
-            molecule = position;
-            denominator = n - position + 1;
-        }else {
-            molecule = n - position +1;
-            denominator = position;
-        }
-
-       String result = molecule + "/" + denominator;
-
-        System.out.println(result);
     }
 }
--- src/이차원배열/Main1.java
+++ src/이차원배열/Main1.java
@@ -6,24 +6,6 @@ public class Main1 {
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-
-        int[][] intArrayA = arrayInput(N, M, sc);
-        int[][] intArrayB = arrayInput(N, M, sc);
-        int[][] intArrayC = sumArray(intArrayA, intArrayB, N, M);
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(intArrayC[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        sc.close();
-
     }
 
     public static int[][] arrayInput(int N, int M, Scanner sc){
@@ -39,15 +21,4 @@ public class Main1 {
         return intArray;
     }
 
-    public static int[][] sumArray(int[][] intArrayA , int[][] intArrayB, int N, int M){
-        int[][] intArrayC = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                intArrayC[i][j]  = intArrayA[i][j] + intArrayB[i][j];
-            }
-        }
-
-        return intArrayC;
-    }
 }
